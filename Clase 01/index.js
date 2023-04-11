53121f8f244af53a59644d34c5faeac0eadd0a94
Matias Velasquez <mvelasquez@lanacion.com.ar>
2023-04-11 09:02:34 -0300

    Clase 1

---
+++ Clase 01/index.js
@@ -0,0 +1,10 @@
+let nombre = prompt("Ingresa tu nombre por favor:");
+let numero = 45;
+let cadena = "Esto es una cadena de texto";
+let bandera = true;
+
+numero = numero + 2;
+
+const pi = 3.14;
+// pi = 4;
+alert(nombre);
